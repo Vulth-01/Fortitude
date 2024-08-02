79c40fdbc54e52e9c26d718dc48e7b831db89f2d
Vulth01 <damienharwood18@gmail.com>
2024-08-02 12:16:56 +0200

    Shader Graphs
    Learning how to add shader graphs

---
+++ Fortitude/Assets/Scripts/STATIC_INFO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class STATIC_INFO
+{
+    public static string gameVersion = "1.0";
+
+    public static string playerUsername = "Player1";
+
+
+
+
+}
